474f435e08716d8b9858957a12b11c26c229fb40
yuriwillian <yuriwilian@gmail.com>
2022-06-27 00:29:42 -0300

    art-luety-gallery

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function abrirMenu() {
+    menuOculto = document.getElementById("menuOculto");
+    menuOculto.style.display = "block"
+}
+  
+  
+function esconderMenu() {
+    menuOculto = document.getElementById("menuOculto");
+    menuOculto.style.display = "none"
+}
